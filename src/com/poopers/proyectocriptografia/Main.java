d86b059eab40e9c158debe4bc5fdcb8f4573f562
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-05-09 18:38:59 -0500

    Se comenzó con la propuesta de pantalla del Emisor

--- src/com/poopers/proyectocriptografia/Main.java
+++ src/com/poopers/proyectocriptografia/Main.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.poopers.proyectocriptografia;
 
-/**
- *
- * @author porfirio
- */
+import com.poopers.proyectocriptografia.pantallas.EmisorArchivoForm;
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EmisorArchivoForm.main(args);
     }
     
 }
